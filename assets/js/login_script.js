9434bc1c4510cf168054ffcdfa9cf328d872d314
George Bublyk <e.bublyk1010@gmail.com>
2019-03-29 16:06:46 +0200

    login functionality added

---
+++ assets/js/login_script.js
@@ -0,0 +1,17 @@
+
+new Vue ({
+    el: '.login_app',
+    data: {
+        email: "",
+        password: "",
+        saveUserToStorage: false
+    },
+    methods: {
+        sendLoginQuery() {
+            console.log(this.email + " " + this.password + " " + this.saveUserToStorage);
+            if(true) {
+                document.location.href = "index.html";
+            }
+        }
+    }
+});
