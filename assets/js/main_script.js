386de6d8aded9996e6571b93f8fb0e8dfe347197
George Bublyk <e.bublyk1010@gmail.com>
2019-03-29 19:35:21 +0200

    Menu bug white-space fixed

--- assets/js/main_script.js
+++ assets/js/main_script.js
@@ -2,19 +2,30 @@
 new Vue({
     el: '.menu_app',
     data: {
-        
+        menuButtons: [
+            { title: "Main", access: ""},
+            { title: "Workspace", access: ""},
+            { title: "Projects", access: ""},
+            { title: "Statistics", access: ""},
+            { title: "Apps", access: ""},
+            { title: "Admin Panel", access: ""},
+            { title: "Blog", access: ""},
+            { title: "Log Out", access: ""} ],
+        show_list: false
     },
     methods: {
         menuTrigger(action) {
             if(action) {
                 this.$refs.menu.style.width = "250px";
                 this.$refs.openMenuButton.style.marginLeft = "250px";
+                this.show_list = true;
                 //document.getElementById("mySidenav").style.width = "250px";
                 //document.getElementById("main").style.marginLeft = "250px";
                 document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
             } else {
                 this.$refs.menu.style.width = "0";
                 this.$refs.openMenuButton.style.marginLeft = "0";
+                this.show_list = false;
                 //document.getElementById("mySidenav").style.width = "0";
                 //document.getElementById("main").style.marginLeft = "0";
                 document.body.style.backgroundColor = "white";
