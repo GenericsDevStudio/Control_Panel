23ee6b3b40d6922703c46b4cbae90144bb3b1181
George Bublyk <e.bublyk1010@gmail.com>
2019-03-29 14:59:05 +0200

    Main libraries added

--- index.php
+++ index.php
@@ -7,15 +7,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     
-    <link rel="stylesheet" href="">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+    <!-- Стили подключать вместо этой строки -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    <title>Generics Control</title>
     
 </head>
 
 <body>
 
+    
+    
 
-    <script type="application/javascript" src=""></script>
+    <script type="application/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+    <script type="application/javascript" src="assets/js/bootstrap.min.js"></script>
+    <!-- Скрипты подключать вместо этой строки -->
+    <script type="application/javascript" src="assets/js/vue.js"></script>
+    <script type="application/javascript" src="assets/js/axios.js"></script>
+    
 </body>
     
 </html>
